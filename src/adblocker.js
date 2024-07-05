b4a96ac5c684afad85e63a58331c2b89b8c100af
ilman01 <ilmanzidni2006@gmail.com>
2024-07-06 00:24:22 +0700

    ignore the errors lol

--- src/adblocker.js
+++ src/adblocker.js
@@ -2,7 +2,7 @@
 // https://www.reddit.com/r/ClubPenguin/comments/171q6tb/guide_how_to_make_newcp_client_fullscreen/
 // https://stackoverflow.com/questions/46562192/electron-execute-javascript-as-very-first-thing-on-a-page
 
-/*
+
 function CheckNotFocused(){
     if (!document.hasFocus()){
         document.exitFullscreen();
@@ -14,9 +14,9 @@ function CheckFocused(){
         document.querySelector("#D_F_GameSection").requestFullscreen()
     }
 }
-setInterval(CheckNotFocused, 500)
-setInterval(CheckFocused, 500)
-*/
+setInterval(CheckNotFocused, 1000)
+setInterval(CheckFocused, 1000)
+
 
 function removeAds() {
     const ads = document.querySelectorAll("[data-google-query-id]");
