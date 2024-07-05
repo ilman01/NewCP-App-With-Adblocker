15c0f9af932e860d70a82743c42e3eddde775379
ilman01 <ilmanzidni2006@gmail.com>
2024-07-06 00:20:29 +0700

    Work in progress

--- src/adblocker.js
+++ src/adblocker.js
@@ -2,6 +2,22 @@
 // https://www.reddit.com/r/ClubPenguin/comments/171q6tb/guide_how_to_make_newcp_client_fullscreen/
 // https://stackoverflow.com/questions/46562192/electron-execute-javascript-as-very-first-thing-on-a-page
 
+/*
+function CheckNotFocused(){
+    if (!document.hasFocus()){
+        document.exitFullscreen();
+        document.activeElement.blur();
+    }
+}
+function CheckFocused(){
+    if (document.activeElement.tagName === 'IFRAME') {
+        document.querySelector("#D_F_GameSection").requestFullscreen()
+    }
+}
+setInterval(CheckNotFocused, 500)
+setInterval(CheckFocused, 500)
+*/
+
 function removeAds() {
     const ads = document.querySelectorAll("[data-google-query-id]");
     ads.forEach(ad => ad.remove());
